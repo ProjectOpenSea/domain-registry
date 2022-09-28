3d0e4ea2804093aa978bb0a12e216126c9331f1e
stephankmin <stephankmin@gmail.com>
2022-09-28 15:36:43 -0400

    address comments

--- contracts/DomainRegistry.sol
+++ contracts/DomainRegistry.sol
@@ -5,21 +5,20 @@ import "./interfaces/DomainRegistryInterface.sol";
 /**
  * @title  DomainRegistry
  * @author stephankmin, 0age
- * @notice DomainRegistry is a public reverse registry of tags (hash values appended to
- *         the salt and/or fulfillment calldata of Seaport orders) and their corresponding domains (keys).
- *         Users can look up a tag from a Seaport order in the registry to find its registered domains.
+ * @notice DomainRegistry is a public reverse registry of tags (hash values included as components to
+ *         function arguments or appended to calldata) and their corresponding domains (keys).
+ *         Users can look up a tag from a transaction in the registry to find its registered domains.
  */
 contract DomainRegistry is DomainRegistryInterface {
     // Returns an array of registered domains for an input tag.
-    mapping(bytes4 => string[]) private registry;
+    mapping(bytes4 => string[]) private _registry;
 
     // Returns true if the domain has already been registered.
-    mapping(string => bool) private hasDomainBeenRegistered;
-
-    constructor() {}
+    mapping(string => bool) private _hasDomainBeenRegistered;
 
     /**
-     * @notice Hash the domain and insert it in the registry.
+     * @notice Hash the domain and insert it in the registry. The call will fail
+     *         if the domain has already been registered.
      *
      * @param domain A string domain to be hashed and inserted into the registry.
      *
@@ -27,21 +26,21 @@ contract DomainRegistry is DomainRegistryInterface {
      */
     function setDomain(string calldata domain) external returns (bytes4 tag) {
         // Revert if the domain has already been registered.
-        if (hasDomainBeenRegistered[domain]) {
+        if (_hasDomainBeenRegistered[domain]) {
             revert DomainAlreadyRegistered(domain);
         }
 
         // Set the domain as having been registered.
-        hasDomainBeenRegistered[domain] = true;
+        _hasDomainBeenRegistered[domain] = true;
 
         // Create the tag by hashing the string domain.
         tag = bytes4(keccak256(abi.encode(domain)));
 
-        // Find the index at which to insert the domain in the registry's domain array.
-        uint256 index = registry[tag].length;
+        // Store the index of the new domain to be emitted in the event.
+        uint256 index = _registry[tag].length;
 
         // Append the domain to the array of domains in the registry.
-        registry[tag][index] = domain;
+        _registry[tag].push(domain);
 
         // Emit an event signaling the domain has been registered.
         emit DomainRegistered(domain, tag, index);
@@ -49,6 +48,7 @@ contract DomainRegistry is DomainRegistryInterface {
 
     /**
      * @notice Get the array of registered domains that are keys for the tag hash value.
+     *         The call may run out of gas depending on the size of the returned array.
      *
      * @param tag The tag to get the registered domains for.
      *
@@ -60,7 +60,7 @@ contract DomainRegistry is DomainRegistryInterface {
         returns (string[] memory domains)
     {
         // Return the string array of registered domains that hash to the input tag.
-        return registry[tag];
+        return _registry[tag];
     }
 
     /**
@@ -76,11 +76,12 @@ contract DomainRegistry is DomainRegistryInterface {
         returns (uint256 totalDomains)
     {
         // Return the total number of registered domains that hash to the input tag.
-        return registry[tag].length;
+        return _registry[tag].length;
     }
 
     /**
-     * @notice Get the domain for a specific tag at a specific index.
+     * @notice Get the domain for a specific tag at a specific index. The call will revert
+     *         if the index is out of range.
      *
      * @param tag   The key value tag to pass into the registry mapping.
      * @param index The index to pass into the array of domains returned by the registry.
@@ -93,14 +94,15 @@ contract DomainRegistry is DomainRegistryInterface {
         returns (string memory domain)
     {
         // Get the number of domains that have been registered for the input tag.
-        uint256 totalDomains = registry[tag].length;
+        uint256 totalDomains = _registry[tag].length;
 
-        // Revert if the passed in index is out of range for the array of domains corresponding to the tag.
+        // Revert if the index parameter is out of range for the array of domains
+        // corresponding to the tag.
         if (index >= totalDomains) {
             revert DomainIndexOutOfRange(tag, totalDomains, index);
         }
 
         // Return the domain for the given tag at the given index.
-        return registry[tag][index];
+        return _registry[tag][index];
     }
 }
