bc0619faf1d1e0869aa07035c3568a7281b23f53
stephankmin <stephankmin@gmail.com>
2022-09-28 14:26:08 -0400

    Revert "update compiler version in config"
    This reverts commit 95f40cc6d8cf5a6f7e2b2dfe7b6d61991690981a.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -27,7 +27,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.17",
+        version: "0.8.14",
         settings: {
           viaIR: true,
           optimizer: {
