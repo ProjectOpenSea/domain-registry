6718ff17a9284784cb1afa3734b6963dbe93a200
stephankmin <stephankmin@gmail.com>
2022-09-30 12:45:06 -0400

    rm waffle

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,7 +6,6 @@ import type { HardhatUserConfig } from "hardhat/config";
 import "dotenv/config";
 import "@nomicfoundation/hardhat-chai-matchers";
 import "@nomiclabs/hardhat-ethers";
-import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-etherscan";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
